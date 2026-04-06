dd10b06ca4b287f97e5755ddc4cf90b1cb3d09dd
zoxilsi <hey.abhijith@gmail.com>
2026-04-06 16:42:54 +0530

    chore: add Vercel Analytics and include Analytics component in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -86,3 +87,4 @@ export default function RootLayout({
     </html>
   );
 }
+          <Analytics />
